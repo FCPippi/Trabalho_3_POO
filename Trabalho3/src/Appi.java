019232b14eba448cb32da09ca0c2e4ec8db81a21
FCPippi <francisco.pippi@edu.pucrs.br>
2022-11-14 07:55:40 -0300

    Changed JFrame title and fields moved to local variables

--- Trabalho3/src/Appi.java
+++ Trabalho3/src/Appi.java
@@ -5,12 +5,10 @@ import javax.swing.*;
  */
 
 public class Appi extends JFrame {
-    private Form1 formUm;
-    private Form2 formDois;
 
     public Appi() {
         super();
-        formDois = new Form2(this);
+        Form2 formDois = new Form2(this);
         this.setContentPane(formDois.getPainel());
         this.setTitle("Trabalho 3");
         this.setDefaultCloseOperation(EXIT_ON_CLOSE);
@@ -18,11 +16,6 @@ public class Appi extends JFrame {
         this.setSize(800,200);
         this.setVisible(true);
     }
-
-    /**
-     * Alterna os paineis da janela
-     * @param painel numero do painel para a troca
-     */
 
 
 
